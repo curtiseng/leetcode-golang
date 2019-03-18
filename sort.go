37d190738b691f7993e788634c6f02c1cf3b5d98
zifeng <zifeng.1024@gmail.com>
2019-03-18 09:40:58 +0800

    sort.

---
+++ sort.go
@@ -0,0 +1,23 @@
+package main
+
+
+func bubbleSort(a []int, n int) {
+	if n <= 1 {
+		return
+	}
+
+	for i := range a{
+		flag := false
+		for j := 0; j < n-i-1; j++ {
+			if a[j] > a[j+1] {
+				a[j], a[j+1] = a[j+1], a[j]
+				flag = true
+			}
+			if !flag {
+				break
+			}
+		}
+	}
+
+
+}
