4e93073b25d26b2d6d98947c75ca823252a60b99
zifeng <zifeng.1024@gmail.com>
2019-03-20 10:10:28 +0800

    sort.

--- sort.go
+++ sort.go
@@ -1,6 +1,7 @@
 package main
 
 
+
 func bubbleSort(a []int, n int) {
 	if n <= 1 {
 		return
@@ -19,6 +20,8 @@ func bubbleSort(a []int, n int) {
 	}
 }
 
+
+
 func insertionSort(a []int, n int) {
 	if n <= 1 {
 		return
@@ -35,4 +38,34 @@ func insertionSort(a []int, n int) {
 		}
 		a[j+1] = value
 	}
+}
+
+
+// 由上到下，快速排序
+func quickSort(a []int) {
+	separateSort(a, 0, len(a)-1)
+}
+func separateSort(a []int, start int, end int) {
+	if start >= end {
+		return
+	}
+	i := partition(a, start, end)
+	separateSort(a, start, i-1)
+	separateSort(a, i+1, end)
+}
+func partition(a []int, start int, end int) int {
+	// 选取最后一位当对比数字
+	pivot := a[end]
+	var i = start
+	for j := start; j < end; j++ {
+		if a[j] < pivot {
+			if !(i == j) {
+				// 交换位置
+				a[i], a[j] = a[j], a[i]
+			}
+			i++
+		}
+	}
+	a[i], a[end] = a[end], a[i]
+	return i
 }
