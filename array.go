584ce71ed40453dd6c1ad9699819b41d4f827e80
zifeng <zifeng.1024@gmail.com>
2019-11-08 10:52:13 +0800

    array.

--- array.go
+++ array.go
@@ -1,11 +1,14 @@
 package main
 
 func removeDuplicates(nums []int) int {
+	if len(nums) < 2 {
+		return len(nums)
+	}
 	var i = 0
 	for j:=1; j< len(nums);j++ {
-		if  nums[i] == nums [j] {
-			nums[i] = nums[j]
+		if  nums[i] != nums [j] {
 			i++
+			nums[i] = nums[j]
 		}
 	}
 	return i+1
