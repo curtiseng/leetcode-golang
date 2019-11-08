ad430eb904b13d435d59316d3a326a0a6bb85e2a
zifeng <zifeng.1024@gmail.com>
2019-11-08 11:03:25 +0800

    add test.

--- array.go
+++ array.go
@@ -1,5 +1,9 @@
 package main
 
+/**
+ * 前指针记录已完成的不重复item，后指针记录下一个不重复值
+ * go语言切片是引用传递，但使用append等函数后可能重新分配地址
+ */
 func removeDuplicates(nums []int) int {
 	if len(nums) < 2 {
 		return len(nums)
--- array_test.go
+++ array_test.go
@@ -1,1 +1,14 @@
 package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestRemoveDuplicates(t *testing.T) {
+	a := []int{1, 1, 2, 2, 3}
+	i := removeDuplicates(a)
+	//insertionSort(a, n)
+	fmt.Printf("i = %v\n", i)
+	fmt.Printf("a = %v", a)
+}
