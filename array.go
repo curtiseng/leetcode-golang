b00bf122ad498f9360e9da3393df33a76b2ef814
zifeng <zifeng.1024@gmail.com>
2019-11-09 14:53:52 +0800

    rotate array.

--- array.go
+++ array.go
@@ -17,3 +17,14 @@ func removeDuplicates(nums []int) int {
 	}
 	return i+1
 }
+
+func rotate(nums []int, k int)  {
+	k = k % len(nums)
+	for i := 0; i < k; i++  {
+		last := nums[len(nums)-1]
+		for j := len(nums)-1; j > 0 ; j-- {
+			nums[j] = nums[j-1]
+		}
+		nums[0] = last
+	}
+}
--- array_test.go
+++ array_test.go
@@ -12,3 +12,11 @@ func TestRemoveDuplicates(t *testing.T) {
 	fmt.Printf("i = %v\n", i)
 	fmt.Printf("a = %v", a)
 }
+
+// 切片是引用类型
+func TestRotate(t *testing.T)  {
+	nums := []int{1,2,3,4,5,6,7}
+	k := 3
+	rotate(nums, k)
+	fmt.Printf("nums = %v\n", nums)
+}
